e6ff516066f4360fabc284f28fbc2dc579c5f6d5
Vipzy <awosanyavictor9@gmail.com>
2026-03-24 12:18:00 +0100

    update LoginPage

--- VII-LEARNIG-HUB/src/pages/LoginPage.jsx
+++ VII-LEARNIG-HUB/src/pages/LoginPage.jsx
@@ -1,4 +1,25 @@
-import { AuthProvider} from '../store/AuthContext';
-import { ProtectedRoute} from '../components/ProtectedRoute';
+import {useState } from 'react';
+import {useNavigate} from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';
 
-import LoginPage from ''
+export default function LoginPage(){
+    const [username, setUsername] = useState ('');
+
+    const navigate = useNavigate();
+
+    const {login} = useAuth();
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        if (username.trim() === '') return;
+
+        login ({ name: username });
+
+        navigate('catalog', {replace: true});
+    };
+
+    return(
+        
+    )
+}
